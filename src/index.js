6d65167e14e66f72e50416f538e4521c7221766f
Alex Rykov <alex.rykov@moduscreate.com>
2016-09-15 17:42:00 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// const myWidget = document.querySelector('my-widget');
+// myWidget.myFunc();
+// var a = myWidget.addEventListener('change', () => console.log('change!!!', myWidget.myFunc()))
+// var b = a == 1;
+//
